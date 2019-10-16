ef03e59235490ee4108b816fc245068d214a1ea5
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2019-10-16 14:05:47 +0200

    changed makefile, with correct flags

--- Makefile.students.mk
+++ Makefile.students.mk
@@ -1,10 +1,10 @@
-CFLAGS_seq		:= -O1 -w -fno-inline-functions -fno-early-inlining -fno-inline-small-functions -fno-tree-loop-optimize
-CFLAGS_simd		:= -O1 -w -fno-inline-functions -fno-early-inlining -fno-inline-small-functions -fno-tree-loop-optimize
-CFLAGS_thread	:= -O1 -w -fno-inline-functions -fno-early-inlining -fno-inline-small-functions -fno-tree-loop-optimize
+CFLAGS_seq		:= -O3 -w -fno-inline-functions -fno-early-inlining -fno-inline-small-functions -fno-tree-loop-optimize -mavx2
+CFLAGS_simd		:= -O3 -w -fno-inline-functions -fno-early-inlining -fno-inline-small-functions -fno-tree-loop-optimize -mavx2
+CFLAGS_thread	:= -O3 -w -fno-inline-functions -fno-early-inlining -fno-inline-small-functions -fno-tree-loop-optimize -mavx2
 
 
-k_nearest_seq: k_nearest_seq.c simpletimer.c parse.c 
-	$(CC) -o k_nearest_seq k_nearest_seq.c simpletimer.c parse.c $(INCLUDES) $(LIBS) $(CFLAGS_seq) $(LDFLAGS) 
+k_nearest_seq: k_nearest_seq.c simpletimer.c parse.c
+	$(CC) -o k_nearest_seq k_nearest_seq.c simpletimer.c parse.c $(INCLUDES) $(LIBS) $(CFLAGS_seq) $(LDFLAGS)
 
 k_nearest_simd: k_nearest_simd.c simpletimer.c parse.c vec.c
 	$(CC) -o k_nearest_simd k_nearest_simd.c simpletimer.c parse.c vec.c $(INCLUDES) $(LIBS) $(CFLAGS_simd) $(LDFLAGS)
@@ -14,5 +14,3 @@ k_nearest_simd_book: k_nearest_simd_book.c simpletimer.c parse.c vec.c
 
 k_nearest_thread: k_nearest_thread.c simpletimer.c parse.c vec.c
 	$(CC) -o k_nearest_thread k_nearest_thread.c simpletimer.c parse.c vec.c $(INCLUDES) $(LIBS) $(CFLAGS_thread) $(LDFLAGS)
-
-
